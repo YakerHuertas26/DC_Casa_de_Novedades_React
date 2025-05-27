33a9f1625b10fee6c08a7df76291c94b45d917bf
YakerHuertas26 <123754558+YakerHuertas26@users.noreply.github.com>
2025-05-26 21:13:46 -0500

    validation form with hook form

--- src/Views/Login.jsx
+++ src/Views/Login.jsx
@@ -1,39 +1,67 @@
 import axios from "axios";
 import { apiAxios } from "../config/apiAxios";
+import { useForm} from "react-hook-form"
+import { InputError } from "../_elements/InputError";
 
 const Login = () => {
-    // optener dotos con azios
-    const login= async (params) => {
+    // hook forms
+    const {register, handleSubmit,formState:{errors},setError}= useForm();
+
+    // funtion del handleSubmit / obtengo la data 
+    const  handleClickLogin= handleSubmit((data)=>{
+        console.log(data,'iniciando')
+    });
+
+    // optener dotos con axios
+    const login= async () => {
         
     }
     
     
     return (
         <div className="p-8 shadow-2xl/50 border-5 border-verde-600 md:mt-10 rounded-2xl md:py-10">
-            <form action="">
+            <form action="" onSubmit={handleClickLogin}>
                 <div>
                     <label htmlFor="userName"
                             className="text-[18px]">
-                        Usuario:</label>
-                    <input 
+                        Nombre de Usuario:</label>
+
+                    <input  
+                    {...register('userName',{
+                        required:{value:true, message:'El campo es requerido'},
+                        minLength:{value:'2', message:'El nombre de usuario debe ser mayor a 2 carácteres'},
+                        maxLength:{value:'15', message:'El nombre de usuario debe ser menos de 20 carácteres'}
+                    })}
                         type="text" 
                         name="userName"
                         id="userName"
                         placeholder="Tu usuario"
-                        className="w-full p-1 md:mt-2 border-1 border-morado-oscuro-800 bg-plomo-bajo-100 focus:outline-none rounded-md"
+                        className={`w-full p-1 md:mt-2 border-1 ${errors.userName?'border-red-600 border-2':'border-morado-oscuro-800 border-2'} bg-plomo-bajo-100 focus:outline-none rounded-md`}
                         />
+                        {errors.userName &&(
+                            <InputError>{errors.userName.message}</InputError>
+                        )}
                 </div>
                 <div className="md:mt-8 mt-3">
                     <label htmlFor="password"
                         className="text-[18px]">
                         Contraseña:</label>
                     <input 
+                        {...register ('password',{
+                        required:{value:true, message:'El campo es requerido'},
+                        minLength:{value:'5', message:'La contraseña deber ser mayor a 5 carácteres'},
+                        maxLength:{value:'15', message:'El contraseña debe ser menor a 20 carácteres'}
+                        })}
+
                         type="password" 
                         name="password"
                         id="password"
                         placeholder="********"
-                        className="w-full p-1 border-1 md:mt-2 focus:outline-none rounded-md border-morado-oscuro-800 bg-plomo-bajo-100"
+                        className={`w-full p-1 md:mt-2 border-1 ${errors.userName?'border-red-600 border-2':'border-morado-oscuro-800 border-2'} bg-plomo-bajo-100 focus:outline-none rounded-md`}
                         />
+                        {errors.password &&(
+                            <InputError>{errors.password.message}</InputError>
+                        )}
                 </div>
 
                 <input 
---
+++ src/_elements/InputError.jsx
@@ -0,0 +1,7 @@
+const InputError = ({children}) => {
+    return ( 
+        <p className="text-red-700 text-md px-2">{children}</p>
+    )
+}
+
+export { InputError };
